2e57de5d65841f9fa6f6ea776ac81f792aa74aff
“claudio-code” <suporte2424@outlook.com>
2023-11-11 02:13:39 -0300

    added doc gtk

--- my-gtk-app-memory-management/src/main.rs
+++ my-gtk-app-memory-management/src/main.rs
@@ -24,12 +24,16 @@ fn build_ui(app: &Application) {
     // cell copy value to another reference
     let mut number = Cell::new(2);
 
+    // when a button is clicked, number should be changed
     button.connect_clicked(move |_| number.set(number.get() + 1));
 
+    // create window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Memory leak")
         .child(&button)
         .build();
+
+    // present window
     window.present();
 }
