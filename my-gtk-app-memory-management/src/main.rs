c9edcff3149dcd6e48337b7dc43b8223a5c3140c
“claudio-code” <suporte2424@outlook.com>
2023-11-11 02:09:41 -0300

    added first example memory management

---
+++ my-gtk-app-memory-management/src/main.rs
@@ -0,0 +1,35 @@
+use std::cell::Cell;
+use::gtk::{prelude::*, Button};
+use::gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "org.cia.soneca";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Memory safe")
+        .margin_bottom(12)
+        .margin_end(12)
+        .margin_start(12)
+        .margin_top(12)
+        .build();
+
+    // cell copy value to another reference
+    let mut number = Cell::new(2);
+
+    button.connect_clicked(move |_| number.set(number.get() + 1));
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Memory leak")
+        .child(&button)
+        .build();
+    window.present();
+}
