4474c12662e233ddac0134b279862361519a5f91
“claudio-code” <suporte2424@outlook.com>
2023-11-18 23:54:33 -0300

    Added example json parser

--- request-async/get-list/src/main.rs
+++ request-async/get-list/src/main.rs
@@ -1,8 +1,35 @@
-fn main() {
-    let body = reqwest::get("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Pokemon {
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ResultBody {
+    count: i32,
+    results: Vec<Pokemon>,
+}
+
+#[tokio::main]
+async fn main()  -> Result<(), Box<dyn std::error::Error>> {
+    let uri = "https://pokeapi.co/api/v2/pokemon";
+    let params = [
+        ("limit", "10"),
+        ("offset", "0"),
+    ];
+    let url = reqwest::Url::parse_with_params(uri, params)?;
+    let body = reqwest::get(url)
         .await?
-        .text()
+        .json::<ResultBody>()
         .await?;
 
-    println!("body = {:?}", body);
+    println!("numers of pokemons {}", body.count);
+
+    for pokemon in &body.results {
+        println!("pokemon name {}", pokemon.name);
+    }
+
+    println!("body = {:?}", body.results);
+    Ok(())
 }
