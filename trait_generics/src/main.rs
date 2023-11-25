6651d7efe10d54c7898ea902f976cee4cf1a7e07
“claudio-code” <suporte2424@outlook.com>
2023-11-25 04:47:19 -0300

    Add example who use generics

--- trait_generics/src/main.rs
+++ trait_generics/src/main.rs
@@ -1,6 +1,37 @@
+use std::ops::{Add, Sub};
+
+#[derive(Debug)]
 struct Point<T> {
     x: T,
     y: T,
+}
+
+impl<T> Sub for Point<T>
+where
+    T: Sub<Output = T>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Point {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl<T> Add for Point<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point {
+            x: self.x + rhs.y,
+            y: self.y + rhs.x,
+        }
+    }
 }
 
 // traits is like interfaces in java
@@ -36,9 +67,18 @@ impl Overview for CoachCourse {
 }
 
 fn main() {
-    let coord_user = Point {x: 5.0, y: 5.0};
-    let coord_seller = Point {x: 't', y: 'w'};
-    
+    let coord_user = Point { x: 5.0, y: 5.0 };
+    let coord_seller = Point { x: 2.1, y: 2.2 };
+
+    let coord_user2 = Point { x: 20.2, y: 20.2 };
+    let coord_seller2 = Point { x: 11.2, y: 11.2 };
+
+    let result1 = coord_seller + coord_user;
+    let result2 = coord_user2 - coord_seller2;
+
+    println!("result1 {:?}", result1);
+    println!("result2 {:?}", result2);
+
     let course1 = DevCourse {
         author: String::from("Claudio"),
         headline: String::from("headline"),
@@ -50,10 +90,14 @@ fn main() {
 
     print_message(&course1);
     print_message(&course2);
+    print_message_over(&course2);
 }
-
 
 fn print_message(overview: &dyn Overview) {
     overview.another_message();
     println!("course {}", overview.overview());
+}
+
+fn print_message_over(over: &impl Overview) {
+    println!("overview {}", over.overview());
 }
