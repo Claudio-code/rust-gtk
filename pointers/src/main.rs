fb124be7ebd98e77982d0d027c2a1aacbd4955c4
“claudio-code” <suporte2424@outlook.com>
2024-03-10 04:14:25 -0300

    Added example of box and deferencing

---
+++ pointers/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let tuple = (12, "apps"); // create on the stack
+    let tuple_box = Box::new(tuple); // create referece pointer on the heap, but was stored on the stack
+    println!("{:?}", tuple_box);
+
+    let x = 5; // has value on the stack
+    let y = &x; // has referece of value on the stack
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    println!("{:?}", y);
+}
